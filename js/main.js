c6732b1017a66f4383246999ab4e2f96ea7bd39e
Robbie <saucy.cooking@gmail.com>
2019-01-06 15:50:15 -0700

    made both units move and detect collision succesfully

--- js/main.js
+++ js/main.js
@@ -12,7 +12,8 @@ const player = {
     towerX: 2,
     towerY: 1,
     unitX:  3,
-    unitY:  1
+    unitY:  1,
+    affinity: 1
 }
 const computer = {
     ID:   `Roberto Roboto`,
@@ -20,15 +21,17 @@ const computer = {
     towerX: 12,
     towerY: 1,
     unitX:  10,
-    unitY:  1
+    unitY:  1,
+    affinity: -1
 }
 
 class Unit {
-    constructor(name, controller, x, y) {
+    constructor(name, controller, x, y, orient) {
         this.name = name;
         this.controller = controller;
         this.x = x;
         this.y = y;
+        this.orient = orient;
         this.isAlive = true;
         this.inRange = false;
     }
@@ -36,8 +39,8 @@ class Unit {
 }
 
 class Fighter extends Unit {
-    constructor(name, controller, x, y) {
-        super(name, controller, x, y)
+    constructor(name, controller, x, y, orient) {
+        super(name, controller, x, y, orient)
         this.hp =             10;
         this.damage =         2;
         this.attackSpeed =    1;
@@ -48,11 +51,11 @@ class Fighter extends Unit {
     }
     render() {
         if (this.controller === 'computer') {
-            $(`.square[x='${this.x}'][y='${this.y}']`).addClass(`enemy`);
+            $(`.square[x='${this.x}'][y='${this.y}']`).addClass(`${this.controller}`);
         }else {
-            $(`.square[x='${this.x}'][y='${this.y}']`).addClass(`player`);
+            $(`.square[x='${this.x}'][y='${this.y}']`).addClass(`${this.controller}`);
         }
-        $(`.square[x='${this.x}'][y='${this.y}']`).addClass(`fighter`);
+        $(`.square[x='${this.x}'][y='${this.y}']`).addClass(`${this.name}`);
     }
     checkMoveSpeed() {
         if (game.timer % this.movementSpeed) {
@@ -60,21 +63,26 @@ class Fighter extends Unit {
         } 
     }
     checkCollision() {
-        const $enemyX = this.x
-        const $theEnemy = $(`.square[x='${$enemyX + 1}'][y='${this.y}']`);
-        if ($theEnemy.hasClass(`enemy`)){
+        const $enemyX = this.x;
+        const $theEnemy = $(`.square[x='${$enemyX + this.orient}'][y='${this.y}']`);
+        //console.log($theEnemy)
+        if ($theEnemy.hasClass(`computer`)) {
             console.log(`collision detected`);
+        } else if ($theEnemy.hasClass(`player`)) {
+            console.log(`enemy has collided with us!`);
         } else {
+            console.log(`${this.controller} has made it to this.move!!`)
             this.move();
         }
     }
     move() {
-        if(this.x < 12) {
-            $(`.square[x='${this.x}'][y='${this.y}']`).removeClass(`fighter`);
-            this.x++;
+        if(this.x < 11 && this.x > 1) {
+            $(`.square[x='${this.x}'][y='${this.y}']`).removeClass(`fighter ${this.controller}`);
+            this.x= this.x + this.orient;
             this.render();
         } 
     }
+    // this function starts the move checks and ultimately the units moving.
     moveInt () {
         setInterval(()=>{
             this.checkCollision();
@@ -83,8 +91,8 @@ class Fighter extends Unit {
 }
 
 class Tower extends Unit {
-    constructor(name, controller, x, y) {
-        super(name, controller, x, y)
+    constructor(name, controller, x, y, orient) {
+        super(name, controller, x, y, orient)
         this.hp =       100;
         this.level =    1;
         this.brix = [];
@@ -105,16 +113,16 @@ const makeTower = (controller) => {
     let newTowerX = controller.towerX;
     let newTowerY = controller.towerY;
     console.log(`${controller}'s towerX: ${newTowerX} and towerY: ${newTowerY}`);
-    const tower = new Tower(`tower`, controller.name, newTowerX, newTowerY);
+    const tower = new Tower(`tower`, controller.name, newTowerX, newTowerY, controller.affinity);
     tower.render();
 } 
 
 const makeFighter = (controller) => {
     let newUnitX = controller.unitX;
     let newUnitY = controller.unitY;
-    const newUnit = new Fighter(`fighter`, controller.name, newUnitX, newUnitY);
+    const newUnit = new Fighter(`fighter`, controller.name, newUnitX, newUnitY, controller.affinity);
     newUnit.render();
-    //newUnit.moveInt();
+    newUnit.moveInt();
 }
 
 const gameBoardSetup = () => {
